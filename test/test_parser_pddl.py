adf2a311c398a3d05354bf4538088fb1046d670e
baptlem <lemaire.baptiste@outlook.fr>
2024-04-05 15:13:04 +0200

    test parser

---
+++ test/test_parser_pddl.py
@@ -0,0 +1,4 @@
+from pddl import parse_domain, parse_problem
+
+domain = parse_domain('plannificateur-deterministe/Groupe3/maze_jumper.pddl')
+problem = parse_problem('plannificateur-deterministe/Groupe3/problems/maze_p0.pddl')
