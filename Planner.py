5520144110bcb29aaf912d368d8b8f3d458a10fa
MatthiasPicard <matthias.picard1@gmail.com>
2024-04-05 16:08:20 +0200

    init OurGraphPlan

--- Planner.py
+++ Planner.py
@@ -2,3 +2,16 @@ from pddl import parse_domain, parse_problem
 
 domain = parse_domain(".\Problems\Groupe3\maze_jumper.pddl")
 problem = parse_problem('.\Problems\Groupe3\problems\maze_p0.pddl')
+
+class OurGraphPlan():
+    
+    def __init__(self, domain, problem):
+        self.actions = domain.actions
+        self.predicates = domain.predicates
+        self.goal = problem.goal
+        self.init = problem.init
+        self.objects = problem.objects
+        
+    
+
+
